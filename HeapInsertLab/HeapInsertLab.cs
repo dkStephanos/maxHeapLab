b86d12b83b060cc2fd64dee2644bc189a4fb35b2
dkStephanos <stephanos@goldmail.etsu.edu>
2019-10-12 15:42:37 -0400

    Added main function for program execution

---
+++ HeapInsertLab/HeapInsertLab.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapInsertLab
+{
+    class HeapInsertLab
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size for the heap:  ");
+            int max_size = Int32.Parse(Console.ReadLine());
+            MaxHeap maxHeap = new MaxHeap(max_size);
+
+            for (int i = 0; i < max_size; i++)
+            {
+                Console.WriteLine("\nEnter a string to be entered into the heap:  ");
+                maxHeap.Insert(Console.ReadLine());
+            }
+
+            maxHeap.HeapSort();
+            maxHeap.Print();
+            Console.ReadLine();
+        }
+    }
+}
